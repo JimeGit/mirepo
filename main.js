0329780046c40281889460579135d22fee12ef44
JimeGit <jimena.b20@gmail.com>
2021-06-03 19:37:21 -0300

    primer clase

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 10
+let num2 = 32
+let miNombre = "Jimena"
+
+console.log("Mi nombre es " + miNombre)
+console.log(num1 + num2)
+
+let nombre = prompt("Ingresa tu nombre")
+console.log(nombre)
+
+let edad = prompt("Ingresa tu edad")
+console.log(edad)
+
+alert("Bienvenido " + nombre + " de " + edad)
